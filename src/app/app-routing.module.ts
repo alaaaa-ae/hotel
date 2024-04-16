032df72d5264e451ca66de1827e261ddcfa92529
romsxz <kyoheitakano1326@gmail.com>
2024-04-16 09:54:48 +0800

    My first commit for Angular 17 Login

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo : 'login',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  // {
+  //   path:'',
+  //   component: LayoutComponent,
+  //   children: [
+  //     {
+  //       path:'dashboard',
+  //       component:DashboardComponent
+  //     }
+  //   ]
+  // }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
