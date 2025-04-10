fd0c047145037c7d4affb166a6272d54e9ae8303
houss <houssamhariss25@gmail.com>
2025-04-10 16:49:25 +0100

    ppp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ChambresComponent } from './chambres/chambres.component';
+import { PanierComponent } from './panier/panier.component';
+import { ContactsComponent } from './contacts/contacts.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { AuthenticationService } from './services/authentication.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    ChambresComponent,
+    PanierComponent,
+    ContactsComponent,
     LoginComponent
   ],
   imports: [
@@ -19,9 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/naviguation/naviguation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation', // Doit correspondre à la balise utilisée dans app.component.html
+  templateUrl: './naviguation.component.html',
+  styleUrls: ['./naviguation.component.css']
+})
+export class NavigationComponent {
+  // Vous pouvez ajouter ici des propriétés ou méthodes pour la logique de navigation
+}
