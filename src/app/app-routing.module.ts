fda5f4a1c73f94d479babd8a1fa6f410a9214113
houss <houssamhariss25@gmail.com>
2025-04-10 17:11:00 +0100

    version 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ChambresComponent } from './chambres/chambres.component';
+import { PanierComponent } from './panier/panier.component';
+import { ContactsComponent } from './contacts/contacts.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'chambres', component: ChambresComponent },
+  { path: 'panier', component: PanierComponent },
+  { path: 'contacts', component: ContactsComponent },
   { path: 'login', component: LoginComponent }
 ];
 
